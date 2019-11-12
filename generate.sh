83514f75edd5775b809fb2d00bdfb13e55aab477
John Goldsmith <john.goldsmith@verys.com>
2019-11-11 16:43:01 -0800

    Update generate script to use OpenAPI

--- generate.sh
+++ generate.sh
@@ -2,53 +2,49 @@
 
 CURRENT_VERSION=$(docker run -i --rm stedolan/jq < package.json '.version')
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-INPUT_SPEC="http://docket-api:3000/v1/docs"
-NAME="@docket/docket.js"
 
-echo "Cleaning..."
+printf "\n⏱  Cleaning...\n"
 rm -rf \
-  "$DIR/.swagger-codegen" \
+  "$DIR/.openapi-generator" \
   "$DIR/dist" \
   "$DIR/docs" \
   "$DIR/src" \
   "$DIR/test" \
   "$DIR/.babelrc" \
-  "$DIR/.swagger-codegen-ignore" \
+  "$DIR/.openapi-generator-ignore" \
   "$DIR/.travis.yml" \
   "$DIR/git_push.sh" \
   "$DIR/mocha.opts" \
   "$DIR/package.json" \
-  "$DIR/package-lock.json" \
   "$DIR/README.md"
-echo "Done."
+printf "✅ Done."
 
-echo "Generating JavaScript SDK from ${INPUT_SPEC}..."
-# swagger-codegen config-help -l javascript
+printf "\n⏱  Generating JavaScript SDK...\n"
 docker run \
   --network=docket-api_default \
   --rm \
   --volume ${PWD}:/local \
-  swaggerapi/swagger-codegen-cli \
+  openapitools/openapi-generator-cli \
   generate \
-    --input-spec $INPUT_SPEC \
-    --lang javascript \
-    --output /local \
-    --template-dir /local/modules/swagger-codegen/src/main/resources/Javascript/es6 \
-    --additional-properties usePromises=true,useES6=true,emitModelMethods=true,projectName='docket-js-sdk',projectDescription='JavaScript SDK for interfacing with the Docket API'
-echo "Done."
+    -i http://docket-api:3001/v1/docs \
+    -g javascript \
+    -o /local \
+    --additional-properties usePromises=true,useES6=true,emitModelMethods=true,projectName='docket.js',projectDescription='JavaScript SDK for interfacing with the Docket API'
+printf "✅ Done."
 
-echo "Updating package.json 'version' to ${CURRENT_VERSION}..."
+printf "\n⏱  Updating package.json 'version' property...\n"
 docker run -i -e CURRENT_VERSION=$CURRENT_VERSION --rm stedolan/jq < package.json ".version = $CURRENT_VERSION" > package.tmp.json && mv package.tmp.json package.json
-echo "Done."
+printf "✅ Done."
 
-echo "Updating package.json 'name' to ${NAME}..."
-docker run -i -e NAME=$NAME --rm stedolan/jq < package.json ".name = \"$NAME\"" > package.tmp.json && mv package.tmp.json package.json
-echo "Done."
-
-echo "Transpiling via Babel..."
+printf "\n⏱  Transpiling via Babel...\n"
 docker run -v ${PWD}:/local --rm -it $(docker build -q .) babel /local/src -d /local/dist
-echo "Done."
+printf "✅ Done."
 
-echo "Updating package.json 'main' to 'dist/index.js'..."
-docker run -i --rm stedolan/jq < ./package.json '.main = "dist/index.js"' > package.tmp.json && mv package.tmp.json package.json
-echo "Done."
+printf "\n⏱  Updating package.json 'main' and 'name' properties...\n"
+docker run -i --rm stedolan/jq < ./package.json '.main = "dist/index.js" | .name = "@docket/docket.js"' > package.tmp.json && mv package.tmp.json package.json
+printf "✅ Done."
+
+printf "\n🎗  Reminder:\n"
+printf "To use this locally generated SDK, in the package.json file of the UI project, change the value of @docket/docket.js to \"file:../docket.js\"\n"
+
+exit 0
